98b4da1e579161898a491ae5d3721a16432cab35
Rroshan12 <grappleroshan7@gmail.com>
2021-11-11 20:39:34 +0545

    scroll bar updated

--- client-app/src/components/Detail/Detail.js
+++ client-app/src/components/Detail/Detail.js
@@ -4,7 +4,7 @@ function Detail({userdetail}) {
     return (
         <div>
          
-         <a href={`${userdetail.html_url}`} class=" black f7 "> {userdetail.name} </a>
+         <a href={`${userdetail.html_url}`} class=" purple f5 "> {userdetail.name} </a>
          <p class="f7 black">#openIssue:<strong class="f5 darkblack" >{userdetail.open_issues_count}</strong>:#def_branch:<strong class="f5 darkblack" >{userdetail.default_branch}</strong></p>
           
        
